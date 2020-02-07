a987d1518ab24109d42746e207e6ee19cac28571
Lucero Toral <lucerotoral@quickenloans.com>
2020-02-06 19:09:48 -0500

    First commit - Exercise 42

---
+++ Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExtraProbs
+{
+    class Point
+    {
+        #region fields
+        private int x;
+        private int y;
+
+        #endregion fields
+
+        #region properties
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        #endregion properties
+
+        #region constructor
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        #endregion constructor
+
+        #region methods
+        public override string ToString()
+        {
+            return $"\nYou have created a point object ({x},{y}).";
+        }
+        #endregion methods
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ClassesExtraProbs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            do
+            {
+                
+                int xCoordinate = GetInt("\n>> Enter an X coordinate: << ");
+
+                int yCoordinate = GetInt(">> Enter an Y coordinate: << ");
+
+                Point userInputs = new Point(xCoordinate, yCoordinate);
+
+                Console.WriteLine(userInputs);
+
+            } while (UserContinue());
+
+            Console.WriteLine("\n>>Goodbye!");
+        }
+        static int GetInt(string prompt)
+        {
+            string input;
+            int number;
+            bool worked;
+
+            Console.Write(prompt);
+            input = Console.ReadLine();
+            worked = int.TryParse(input, out number);
+
+            while (worked == false)
+            {
+                Console.WriteLine("Let's try again with a whole number.");
+                Console.Write($"\n{prompt}");
+                input = Console.ReadLine();
+
+                worked = int.TryParse(input, out number);
+            }
+            return number;
+        }
+        static bool UserContinue()
+        {
+            char key;
+            do
+            {
+                Console.Write("\nWould you like to continue (y/n)? <<");
+                key = Console.ReadKey().KeyChar;
+                key = char.ToLower(key);
+                if (key == 'n')
+                {
+                    return false;
+                }
+                Console.WriteLine();
+
+            } while (key != 'y');
+            return true;
+        }
+    }
+}
